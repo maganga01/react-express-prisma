b4a70fe0b9a1bfddc7bed755dac7760edc2cc856
maganga01 <code4chris.com>
2023-12-18 21:49:26 +0200

    add

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+
+
+function App() {
+  
+
+  return (
+    <>
+      <div className='flex items-center p-10'>
+        <div className='bg-gray-300 text-center'>
+          prisma
+
+        </div>
+
+      </div>
+        
+    </>
+  )
+}
+
+export default App
